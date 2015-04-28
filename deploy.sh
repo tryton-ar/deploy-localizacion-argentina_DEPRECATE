ef6c329bae2af05602c8612a3346ab7a8a5a589d
Luciano Rossi <lukio@gcoop.coop>
2015-04-28 16:44:13 -0300

    Terminado el script de deploy para version 3.4. Falta el scenario_base.py

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,17 @@
 # Se loggea en /var/log/syslog
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
+# exit if using uninitialized variable
+# set -u
+# exit on error
+# set -e
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` -t 3.4 -c /etc/trytond.conf --database-host localhost"
+  exit 0
+fi
+
+VERSION="3.4"
 while [[ $# > 1 ]]
 do
 key="$1"
@@ -16,7 +27,7 @@ case $key in
     TRYTON_CONFIG_FILE="$2"
     shift
     ;;
-    --database-host)
+    -d|--database-host)
     DATABASE_HOST="$2"
     shift
     ;;
@@ -39,12 +50,12 @@ echo "---------------------------------------------------------"
 VIRTUALENV="tryton-localizacion-ar"
 DATABASE="tryton_ar"
 
-if [ ! -f $VERSION ]; then
+if [ -z "$VERSION" ]; then
     echo "Debemos definir que version de Tryton queremos instalar" >&2
     echo "Por defecto usamos la version: 3.4" >&2
-    VERSION="3.4"
     exit 1
 fi
+
 
 DATABASE_NAME=${DATABASE}_${VERSION}
 DATABASE_NAME=${DATABASE_NAME//./_}
@@ -52,7 +63,7 @@ DATABASE_NAME=${DATABASE_NAME////_}
 
 source `which virtualenvwrapper.sh`
 
-if [ ! -f $TRYTON_CONFIG_FILE ]; then
+if [ ! -e "$TRYTON_CONFIG_FILE" ]; then
     echo "El archivo de configuracion de trytond no existe $TRYTON_CONFIG_FILE" >&2
     exit 1
 fi
@@ -70,26 +81,46 @@ echo "---------------------------------------------------------"
 echo "Ingresamos al virtualenv"
 echo "---------------------------------------------------------"
 workon $VIRTUALENV
+INSTALACION_ACTUAL=`pwd`
 
 echo "---------------------------------------------------------"
 echo "Comenzamos instalacion de paquetes pip"
 echo "---------------------------------------------------------"
-pip install -r requirements-trytond-3.4.txt
+pip install -r requirements-trytond-$VERSION.txt
+
+cdsitepackages
+cd ../../../src
+MODULES_EXTRA=`pwd`
 
 echo "---------------------------------------------------------"
 echo "Instalar pyafipws"
 echo "---------------------------------------------------------"
 
-cdsitepackages
 wget https://github.com/reingart/pyafipws/archive/2.7.tar.gz 
 tar zxvf 2.7.tar.gz
-mv pyafipws-2.7 pyafipws
 rm 2.7.tar.gz
+
+echo "---------------------------------------------------------"
+echo "Instalamos modulos locales en el virtualen"
+echo "---------------------------------------------------------"
+
+cdsitepackages
+cd trytond/modules
+MODULES_TRYTON=`pwd`
+
+echo "---------------------------------------------------------"
+echo "Creamos symbolic links de los modulos extras"
+echo "---------------------------------------------------------"
+ln -v -s $MODULES_EXTRA/account-ar $MODULES_TRYTON/account_ar
+ln -v -s $MODULES_EXTRA/account-invoice-ar $MODULES_TRYTON/account_invoice_ar
+ln -v -s $MODULES_EXTRA/trytond-company-logo $MODULES_TRYTON/company_logo
+ln -v -s $MODULES_EXTRA/pyafipws-2.7 $MODULES_TRYTON/pyafipws
 
 echo "---------------------------------------------------------"
 echo "Ejecutamos scripts de instalacion de scenario base"
 echo "---------------------------------------------------------"
-python2 scenario_base.py -d $DATABASE_NAME -c $TRYTON_CONFIG_FILE -a admin -l $DATABASE_HOST
+cd $INSTALACION_ACTUAL
+python2 scenario_base.py -d $DATABASE_NAME -c $TRYTON_CONFIG_FILE -l $DATABASE_HOST
 
 echo "----------------------------------------------------------------"
 echo "Finalizamos deploy de instalacion TRYTON Localizacion Argentina."
