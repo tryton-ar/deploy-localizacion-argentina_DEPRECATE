5fccfbebb85adc77b9bff5c31117b12ebc89c7a7
Luciano Rossi <lukio@gcoop.coop>
2015-04-30 17:10:34 -0300

    Modificamos para poder instalar los modulos de la localizacion argentina usando pypi

--- deploy.sh
+++ deploy.sh
@@ -112,10 +112,6 @@ MODULES_TRYTON=`pwd`
 echo "---------------------------------------------------------"
 echo "Creamos symbolic links de los modulos extras"
 echo "---------------------------------------------------------"
-ln -v -s $MODULES_EXTRA/account-ar $MODULES_TRYTON/account_ar
-ln -v -s $MODULES_EXTRA/account-invoice-ar $MODULES_TRYTON/account_invoice_ar
-ln -v -s $MODULES_EXTRA/trytond-company-logo $MODULES_TRYTON/company_logo
-ln -v -s $MODULES_EXTRA/account_coop_ar $MODULES_TRYTON/account_coop_ar
 ln -v -s $MODULES_EXTRA/pyafipws-2.7 $SITEPACKAGES/pyafipws
 
 echo "---------------------------------------------------------"
